ed12980bfff6c8c33760c6b1ddaae3bc12b4b1ac
ravi2004 <Ravi_kanchan2004@yahoo.com>
2023-08-27 21:59:56 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/api/baseapi.js 	modified:   src/index.tsx 	modified:   src/setupProxy.js 	modified:   tsconfig.json

--- src/api/baseapi.js
+++ src/api/baseapi.js
@@ -1,8 +1,4 @@
 class baseApi {
-  /*
-    constructor(option) {
-    };
-*/
     async fetchData(url, option) {
         return await fetch(process.env.REACT_APP_REST_URL + '/' + url, option)
             .then((res) => {
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,4 +8,7 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
+
+
+  
 };
